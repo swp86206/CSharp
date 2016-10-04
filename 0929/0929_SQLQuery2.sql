ee7f958f8eec8b859e9a205260428f1530436d6a
Your Name <you@example.com>
2016-10-04 08:05:21 +0800

    1003 at night

---
+++ 0929/0929_SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE LabTable
+(
+  id int NOT NULL PRIMARY KEY,
+  data varchar(20)
+)
+GO
+
+insert into LabTable values (1, 'data a')
+insert into LabTable values (2, 'data b')
+insert into LabTable values (3, 'data c')
+Go
+
+CREATE USER [Users] FOR LOGIN [BUILTIN\Users]
+ALTER ROLE [db_datareader] ADD MEMBER [Users]
+ALTER ROLE [db_datawriter] ADD MEMBER [Users]
+GO
+
+ select * from LabTable
